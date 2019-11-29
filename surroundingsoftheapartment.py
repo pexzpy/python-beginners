873dd6c8111d780f6e0104fae505edfe26b22c96
pexzpy <58317205+pexzpy@users.noreply.github.com>
2019-11-29 06:05:42 +0300

    Create surroundingsoftheapartment.py

---
+++ surroundingsoftheapartment.py
@@ -0,0 +1,15 @@
+pi = 3.14
+
+
+#input(retrieve data from the user)
+#int(Convert 'int' to data type)
+#r(Sync to r)
+r = int(input("Please enter the radius of the circle :: "))
+
+
+#find its surroundings
+circum = 2*pi*r
+
+
+#print(output)
+print("Circumference of the apartment :: ",circum)
