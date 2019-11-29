5e736243e660a72c441dc616526ef0fc4f6825c9
pexzpy <58317205+pexzpy@users.noreply.github.com>
2019-11-29 06:07:56 +0300

    Create areaof​​theapartment.py

---
+++ areaof​​theapartment.py
@@ -0,0 +1,14 @@
+pi = 3.14
+
+#input(retrieve data from the user)
+#int(Convert 'int' to data type)
+#r(Sync to r)
+r = int(input("Please enter the radius of the circle :: "))
+
+
+#find its area
+area = pi*yaricap*r
+
+
+#print(output)
+print("Area of the apartment :: ",area)
